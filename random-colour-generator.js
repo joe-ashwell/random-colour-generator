6293d6f9fe17211041e93980d3a03bf3f11df6b7
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-08-11 18:24:53 +0100

    Add back button w/ limit

--- random-colour-generator.js
+++ random-colour-generator.js
@@ -1,21 +1,75 @@
 const generateColourButton = document.querySelector('button.randomColourGenerator');
+const goBack = document.querySelector('button.goBack');
 const hexValues = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const body = document.querySelector('body.randomColourGenerator');
+let memory = [];
 
-generateColourButton.addEventListener('click', function() {
-  
+let generateRandomFunction = () => {
+
   let hexCodeValue;
   let hexCode = [];
 
+  // Loops through the hexValues array and adds 6 random value into hexCode
   for (let i = 0; i < 6; i++) {
     hexCodeValue = Math.floor(Math.random() * hexValues.length);
     hexCode.push(hexValues[hexCodeValue]);
   }
 
+  // This is then joined into a string
   hexString = hexCode.join('');
-  body.style.backgroundColor = "#" + hexString;
 
+  // Then added to hexColour with added # value
+  let hexColour = "#" + hexString;
+
+  // Adds to memory
+  memory.push(hexColour);
+
+  // Sets background colour
+  body.style.backgroundColor = hexColour;
+
+  // Display value
   let p = document.querySelector('p.randomColourGenerator');
-  p.innerHTML = "#" + hexString;
+  p.innerHTML = hexColour;
+
+}
+
+let disabledTrueFalse = () => {
+
+  if ( memory.length <= 1 ) {
+    goBack.disabled = true;
+    goBack.classList.add("disabled");
+  }  else {
+    goBack.disabled = false;
+    goBack.classList.remove("disabled");
+  }
+
+}
+
+generateColourButton.addEventListener('click', () => {
+  
+  generateRandomFunction();
+
+  disabledTrueFalse();
+
+});
+
+
+// Calls the getWord function on load
+window.addEventListener('load', () => {
+
+  disabledTrueFalse();
+
+});
+
+goBack.addEventListener('click', () => {
+
+  disabledTrueFalse();
+
+  let lastItem = memory.pop();
+
+  body.style.backgroundColor = lastItem;
+  p.innerHTML = lastItem;
+
+    
 
 });
